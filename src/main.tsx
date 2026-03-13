82e7886260ed3c1ce3da50839648d6af510e6c84
Erik Greyan <erikgreyan.dev@gmail.com>
2026-03-13 10:45:28 +0400

    Add landing page, analytics, SEO setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// Analytics
 import "./analytics/analytics";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
